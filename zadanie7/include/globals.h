37703503dd0c41ffda7cda4d0330b8df411facc3
xSzymonKietlinskix <szymon.kietlinski@gmail.com>
2025-01-17 20:27:55 +0100

    changed global parametres

--- zadanie7/include/globals.h
+++ zadanie7/include/globals.h
@@ -2,12 +2,12 @@
 #ifndef GLOBALS_H
 #define GLOBALS_H
 
-constexpr int populationSize = 300;
-constexpr int numGenerations = 300;
-const int numVariables = 2;
-const double mutationRate = 0.4;
-const double mutationStrength = 0.1;
-const int archiveSize = 300;
+constexpr int populationSize = 500;
+constexpr int numGenerations = 1000;
+const int numVariables = 2; //must be 2 for ZDT1 (it's m)
+const double mutationRate = 0.8;
+const double mutationStrength = 0.7;
+const int archiveSize = 500;
 const int tournamentSize = 5;
 const int densityNeighbours = 5;
 const double crossoverStrength = 0.5;
